3706cd7a123ab1551946b51440a17328e1b1b012
Paulo Victor <paulovictordev01@gmail.com>
2026-01-28 00:00:23 -0300

    Tuplas no typescript

---
+++ tuplas/tuplas.ts
@@ -0,0 +1,8 @@
+let crew: String[] // arr de strings
+let adress: [string, string, number] // arr de apenas 2 strings
+
+adress = ['rua A', 'bairro tal', 800]
+
+let [x, y, z] = adress //retornando tuplas
+//let x = crewOfTwo[0]
+//let y = crewOfTwo[1]
