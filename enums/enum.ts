0019afcbf114bc2e96457ed04f51ac0d7085fa88
Paulo Victor <paulovictordev01@gmail.com>
2026-01-31 23:27:00 -0300

    Enums no typescript, comentando codigo

--- enums/enum.ts
+++ enums/enum.ts
@@ -1,6 +1,18 @@
-enum planets {
-    mercury,
+enum Planets {
+    mercury = 1, //inicialmente é 0, valores incrementam automaticamente
     venus,
     earth,
     mars
 }
+
+Planets.earth
+
+const Cores = {
+  Sucesso: "green",
+  Erro: "red"
+} 
+
+Cores.Sucesso = 'Blue'
+
+Cores.Sucesso // result - (property) Sucesso: string
+//dependendo é melhor utilizar objeto com as const
