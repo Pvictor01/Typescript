6e503e8d1cf7838220f456a4f4b2e2562eb2d7e4
Paulo Victor <paulovictordev01@gmail.com>
2026-02-02 23:35:18 -0300

    Evitando tipagem, exemplo do unknown

--- evitando-tipagem/evitando-tipagem.ts
+++ evitando-tipagem/evitando-tipagem.ts
@@ -1,6 +1,19 @@
-function sendCar(car: {driver: string, speed?: number}) { // sinal de ? diz que o parametro é opcional
+/*function sendCar(car: {driver: string, speed?: number}) { // sinal de ? diz que o parametro é opcional
 
 }
 
 sendCar({driver: 'Vitor', speed: 100})
-sendCar({driver: 'Vitor'}) //com o ? a funçao aceita apenas 1 parametro
+sendCar({driver: 'Vitor'}) //com o ? a funçao aceita apenas 1 parametro*/
+
+let input: unknown // any desabilita o tsc, codigo fica só com js
+
+input = 10
+input = '3'
+input = []
+
+let text: string
+
+text = input //da erro pois o ts nao aceita tipo especifico no unknown
+
+console.log(text)
+
