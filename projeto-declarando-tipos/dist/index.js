46fd30d54de0649f01d1013cf2a4614fdf620928
Paulo Victor <paulovictordev01@gmail.com>
2026-02-06 00:24:36 -0300

    Criando menu da aplicação, adequando opções

--- projeto-declarando-tipos/dist/index.js
+++ projeto-declarando-tipos/dist/index.js
@@ -1,16 +1,39 @@
-function createSpaceship(spaceshipName, pilot, crewLimit, crew, inMission) {
-    var spaceship = {
-        spaceshipName: spaceshipName,
-        pilot: pilot,
-        crewLimit: crewLimit,
-        crew: crew,
-        inMission: inMission
-    };
-    return spaceship;
+var spaceships = [];
+function menu() {
+    var menu = prompt("\n    Menu Pricipal - Envio de naves\n    \n1- Cadastrar nova nave\n    \n2- Listar naves cadastradas\n    \n3- Enviar nave para miss\u00E3o\n    \n0- Sair\n  ");
+    return menu;
 }
-var spaceshipName = 'Tardis';
-var spaceshipPilot = 'Doctor Who';
-var crewLimit = 2;
-var crew = [];
-var inMission = false;
-console.log(createSpaceship(spaceshipName, spaceshipPilot, crewLimit, crew, inMission));
+var userOption = '';
+do {
+    userOption = menu();
+    switch (userOption) {
+        case '1':
+            function createSpaceship(spaceshipName, pilot, crewLimit, crew, inMission) {
+                var spaceship = {
+                    spaceshipName: spaceshipName,
+                    pilot: pilot,
+                    crewLimit: crewLimit,
+                    crew: crew,
+                    inMission: inMission
+                };
+                return spaceship;
+            }
+            var spaceshipName = prompt('Qual o nome da nave?');
+            var spaceshipPilot = prompt('Qual o nome do piloto?');
+            var crewLimit = Number(prompt('Quantos tripulantes a nave suporta?'));
+            var crew = [];
+            var inMission = false;
+            var sendSpaceship = createSpaceship(spaceshipName, spaceshipPilot, crewLimit, crew, inMission);
+            spaceships.push(sendSpaceship);
+            alert("A nave ".concat(sendSpaceship.spaceshipName, " foi cadastrada com sucesso!"));
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '0':
+            break;
+        default:
+    }
+} while (userOption !== '0');
+console.log(spaceships);
--- projeto-declarando-tipos/index.ts
+++ projeto-declarando-tipos/index.ts
@@ -1,22 +1,59 @@
-function createSpaceship(spaceshipName: string, pilot: string, crewLimit: number, crew: string[], inMission: false) {
-  const spaceship = {
-    spaceshipName,
-    pilot,
-    crewLimit,
-    crew,
-    inMission
-  }
+const spaceships = []
 
-  return spaceship
+function menu() {
+  const menu = prompt(`
+    Menu Pricipal - Envio de naves
+    \n1- Cadastrar nova nave
+    \n2- Listar naves cadastradas
+    \n3- Enviar nave para missão
+    \n0- Sair
+  `)
+
+  return menu
 }
 
-const spaceshipName = 'Tardis'
-const spaceshipPilot = 'Doctor Who'
-const crewLimit = 2
-const crew = []
-const inMission = false
+let userOption = ''
+
+do {
+  userOption = menu()
+  
+  switch (userOption) {
+    case '1':
+      function createSpaceship(spaceshipName: string, pilot: string, crewLimit: number, crew: string[], inMission: false) {
+        const spaceship = {
+          spaceshipName,
+          pilot,
+          crewLimit,
+          crew,
+          inMission
+        }
+      
+        return spaceship
+      }
+
+      const spaceshipName = prompt('Qual o nome da nave?')
+      const spaceshipPilot = prompt('Qual o nome do piloto?')
+      const crewLimit = Number(prompt('Quantos tripulantes a nave suporta?'))
+      const crew = []
+      const inMission = false
+
+      const sendSpaceship = createSpaceship(spaceshipName, spaceshipPilot, crewLimit, crew, inMission)
+
+      spaceships.push(sendSpaceship)
+
+      alert(`A nave ${sendSpaceship.spaceshipName} foi cadastrada com sucesso!`)
+      break
+    case '2':
+      break
+    case '3':
+      break
+    case '0':
+    break;
+    default:
+      
+  }
+} while (userOption !== '0')
 
 
 
-
-console.log(createSpaceship(spaceshipName, spaceshipPilot, crewLimit, crew, inMission))
+console.log(spaceships)
